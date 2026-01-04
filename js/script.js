e18b2a073f3622710b68e23255a030d2a76a337c
mitko <mitko464@gmail.com>
2026-01-04 23:08:08 +0200

    add movie/shows details page

--- js/script.js
+++ js/script.js
@@ -2,26 +2,6 @@ const global = {
     currentPage: window.location.pathname
 }
 
-
-// Highlight active link
-function highlightActiveLink() {
-    const links = document.querySelectorAll(".nav-link");
-    links.forEach(link => {
-        if (link.getAttribute('href') === global.currentPage) {
-            link.classList.add('active')
-        }
-    });
-}
-
-// Show spinner
-function showSpinner() {
-    document.querySelector('.spinner').classList.add('show');
-}
-// Hide spinner
-function hideSpinner() {
-    document.querySelector('.spinner').classList.remove('show');
-}
-
 // Fetch data from TMDB API
 async function fetchAPIdata(endpoint) {
     const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMzI3ZTIxYzczYWU1ODlmZGEzZDBkYWIwOTFkNzZjOSIsIm5iZiI6MTc2NzU0Nzk5Ny4zMTMsInN1YiI6IjY5NWFhNDVkMTljY2MyZjU5MjNiZTE5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0rq2eNwfxya9GfSVl70WZKL2W7oqX0kYQc6_iXdXazo';
@@ -80,7 +60,8 @@ async function displayMovieDetails() {
 
     const movie = await fetchAPIdata(`movie/${movieId}`);
     const moviePoster = `https://image.tmdb.org/t/p/w500/${movie.poster_path}` || '../images/no-image.jpg';
-    console.log(movie)
+
+    displayBackdropImage('movie', movie.backdrop_path);
 
     const movieDetailsElement = document.createElement('div');
 
@@ -124,7 +105,6 @@ async function displayMovieDetails() {
     `;
 
     document.querySelector('#movie-details').appendChild(movieDetailsElement);
-
 }
 
 // Display popular shows
@@ -156,10 +136,105 @@ async function displayPopularShows() {
     })
 }
 
+// Display show details
+async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1];
+
+    const show = await fetchAPIdata(`tv/${showId}`);
+    const showPoster = `https://image.tmdb.org/t/p/w500/${show.poster_path}` || '../images/no-image.jpg';
+    console.log(show)
+
+    displayBackdropImage('tv', show.backdrop_path);
+
+    const showDetailsElement = document.createElement('div');
+
+    showDetailsElement.innerHTML = `
+        <div class="details-top">
+            <div>
+                <img
+                    src="${showPoster}"
+                    class="card-img-top"
+                    alt="${show.name}"
+            />
+            </div>
+            <div>
+                <h2>${show.name}</h2>
+                <p>
+                    <i class="fas fa-star text-primary"></i>
+                    ${show.vote_average.toFixed(1)}
+                </p>
+                <p class="text-muted">First Air Date: ${show.first_air_date}</p>
+                <p>${show.overview}</p>
+                <h5>Genres</h5>
+                <ul class="list-group">
+                    ${show.genres.map(genre => `<li>${genre.name}</li>`).join("")}
+                </ul >
+        <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+            </div >
+        </div >
+        <div class="details-bottom">
+            <h2>Movie Info</h2>
+            <ul>
+                <li><span class="text-secondary">Last Air Date:</span> ${show.last_air_date}</li>
+                <li><span class="text-secondary">Last Episode On Air:</span> ${show.last_episode_to_air.name}</li>
+                <li><span class="text-secondary">Runtime:</span> ${show.runtime || 'N/A'} </li>
+                <li><span class="text-secondary">Status:</span> ${show.status}</li>
+            </ul>
+            <h4>Production Companies</h4>
+            <div class="list-group">
+                ${show.production_companies.map(company => `<span>${company.name}</span>`).join(', ')}
+            </div>
+        </div>
+    `;
+
+    document.querySelector('#show-details').appendChild(showDetailsElement);
+
+}
+
+// Highlight active link
+function highlightActiveLink() {
+    const links = document.querySelectorAll(".nav-link");
+    links.forEach(link => {
+        if (link.getAttribute('href') === global.currentPage) {
+            link.classList.add('active')
+        }
+    });
+}
+
+// Show spinner
+function showSpinner() {
+    document.querySelector('.spinner').classList.add('show');
+}
+// Hide spinner
+function hideSpinner() {
+    document.querySelector('.spinner').classList.remove('show');
+}
 
 // Add commas to number
 function addCommasToNuber(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// Display backdropImage
+function displayBackdropImage(type, imgUrl) {
+    const overlayImage = document.createElement('div');
+    overlayImage.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${imgUrl})`;
+    overlayImage.style.backgroundSize = "cover";
+    overlayImage.style.backgroundPosition = "center";
+    overlayImage.style.backgroundRepeat = "no-repeat";
+    overlayImage.style.height = "100vh";
+    overlayImage.style.width = "100vw";
+    overlayImage.style.position = "absolute";
+    overlayImage.style.top = "0";
+    overlayImage.style.left = "0";
+    overlayImage.style.zIndex = "-1";
+    overlayImage.style.opacity = "0.1";
+
+    if (type === 'movie') {
+        document.querySelector("#movie-details").appendChild(overlayImage)
+    } else {
+        document.querySelector("#show-details").appendChild(overlayImage)
+    }
 }
 
 // App init
@@ -173,7 +248,7 @@ function init() {
             displayPopularShows();
             break;
         case "/tv-details.html":
-            console.log("TV Details page");
+            displayShowDetails();
             break;
         case "/movie-details.html":
             displayMovieDetails();
