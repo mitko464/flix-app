71aa012a117ceddbb89af378727993664f5d1c36
mitko <mitko464@gmail.com>
2026-01-04 21:25:33 +0200

    add popular movies section

--- js/script.js
+++ js/script.js
@@ -13,11 +13,60 @@ function highlightActiveLink() {
     });
 }
 
+// Fetch data from TMDB API
+async function fetchAPIdata(endpoint) {
+    const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMzI3ZTIxYzczYWU1ODlmZGEzZDBkYWIwOTFkNzZjOSIsIm5iZiI6MTc2NzU0Nzk5Ny4zMTMsInN1YiI6IjY5NWFhNDVkMTljY2MyZjU5MjNiZTE5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0rq2eNwfxya9GfSVl70WZKL2W7oqX0kYQc6_iXdXazo';
+    const API_URL = 'https://api.themoviedb.org/3/'
+
+    const options = {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${API_TOKEN}`
+        }
+    }
+
+    const response = await fetch(`${API_URL}${endpoint}?language=en-US`, options);
+    const data = await response.json();
+
+    return data;
+}
+
+// Display popular movies
+async function displayPopularMovies() {
+    const { results } = await fetchAPIdata('movie/popular');
+
+    results.forEach(movie => {
+        const movilePoster = `https://image.tmdb.org/t/p/w500/${movie.poster_path}` || '../images/no-image.jpg';
+        const movieElement = document.createElement('div');
+
+        movieElement.classList.add('card');
+        movieElement.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+                <img
+                src="${movilePoster}"
+                class="card-img-top"
+                alt="${movie.title}"
+                />
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${movie.title}</h5>
+                <p class="card-text">
+                <small class="text-muted">Release: ${movie.release_date}</small>
+                </p>
+            </div>
+        `;
+
+        document.querySelector('#popular-movies').appendChild(movieElement);
+    })
+}
+
+// App init
 function init() {
     switch (global.currentPage) {
         case "/":
         case "/index.html":
-            console.log("Homepage");
+            displayPopularMovies();
             break;
         case "/shows.html":
             console.log("Shows page");
@@ -33,6 +82,7 @@ function init() {
             break;
         default:
             console.log("404 Page Not Found");
+            break;
     }
 
     highlightActiveLink();
