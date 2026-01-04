a02ec52d3fb6fe912ea4d492c360e15a3ea77102
mitko <mitko464@gmail.com>
2026-01-04 19:28:35 +0200

    add app router

--- js/script.js
+++ js/script.js
@@ -1,13 +1,41 @@
-const banica = 123;
-const korem = "sadfasd";
-
-console.log(banica, korem);
-
-alert("Hello, world!");
-
-
-function greet(name) {
-    return `Hello, ${name}!`;
+const global = {
+    currentPage: window.location.pathname
 }
 
-console.log(greet("User"));
+
+// Highlight active link
+function highlightActiveLink() {
+    const links = document.querySelectorAll(".nav-link");
+    links.forEach(link => {
+        if (link.getAttribute('href') === global.currentPage) {
+            link.classList.add('active')
+        }
+    });
+}
+
+function init() {
+    switch (global.currentPage) {
+        case "/":
+        case "/index.html":
+            console.log("Homepage");
+            break;
+        case "/shows.html":
+            console.log("Shows page");
+            break;
+        case "/tv-details.html":
+            console.log("TV Details page");
+            break;
+        case "/movie-details.html":
+            console.log("Movie Details page");
+            break;
+        case "/search.html":
+            console.log("Search page");
+            break;
+        default:
+            console.log("404 Page Not Found");
+    }
+
+    highlightActiveLink();
+}
+
+document.addEventListener("DOMContentLoaded", init);
