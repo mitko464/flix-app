63490ed6e90b84d1717fa05d2130c3ba0c4f530d
mitko <mitko464@gmail.com>
2026-01-04 19:28:24 +0200

    add app router

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const banica = 123;
+const korem = "sadfasd";
+
+console.log(banica, korem);
+
+alert("Hello, world!");
+
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("User"));
