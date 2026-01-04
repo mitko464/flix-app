7db5c65a52ee901e9b64e4742abfed2e416a2b80
mitko <mitko464@gmail.com>
2026-01-04 21:39:00 +0200

    add popular shows page

--- js/script.js
+++ js/script.js
@@ -13,6 +13,15 @@ function highlightActiveLink() {
     });
 }
 
+// Show spinner
+function showSpinner() {
+    document.querySelector('.spinner').classList.add('show');
+}
+// Hide spinner
+function hideSpinner() {
+    document.querySelector('.spinner').classList.remove('show');
+}
+
 // Fetch data from TMDB API
 async function fetchAPIdata(endpoint) {
     const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMzI3ZTIxYzczYWU1ODlmZGEzZDBkYWIwOTFkNzZjOSIsIm5iZiI6MTc2NzU0Nzk5Ny4zMTMsInN1YiI6IjY5NWFhNDVkMTljY2MyZjU5MjNiZTE5ZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0rq2eNwfxya9GfSVl70WZKL2W7oqX0kYQc6_iXdXazo';
@@ -26,8 +35,12 @@ async function fetchAPIdata(endpoint) {
         }
     }
 
+    showSpinner();
+
     const response = await fetch(`${API_URL}${endpoint}?language=en-US`, options);
     const data = await response.json();
+
+    hideSpinner();
 
     return data;
 }
@@ -37,14 +50,14 @@ async function displayPopularMovies() {
     const { results } = await fetchAPIdata('movie/popular');
 
     results.forEach(movie => {
-        const movilePoster = `https://image.tmdb.org/t/p/w500/${movie.poster_path}` || '../images/no-image.jpg';
+        const moviePoster = `https://image.tmdb.org/t/p/w500/${movie.poster_path}` || '../images/no-image.jpg';
         const movieElement = document.createElement('div');
 
         movieElement.classList.add('card');
         movieElement.innerHTML = `
             <a href="movie-details.html?id=${movie.id}">
                 <img
-                src="${movilePoster}"
+                src="${moviePoster}"
                 class="card-img-top"
                 alt="${movie.title}"
                 />
@@ -61,6 +74,35 @@ async function displayPopularMovies() {
     })
 }
 
+// Display popular shows
+async function displayPopularShows() {
+    const { results } = await fetchAPIdata('tv/popular');
+
+    results.forEach(show => {
+        const showPoster = `https://image.tmdb.org/t/p/w500/${show.poster_path}` || '../images/no-image.jpg';
+        const showElement = document.createElement('div');
+
+        showElement.classList.add('card');
+        showElement.innerHTML = `
+            <a href="tv-details.html?id=${show.id}">
+                <img
+                src="${showPoster}"
+                class="card-img-top"
+                alt="${show.name}"
+                />
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${show.name}</h5>
+                <p class="card-text">
+                <small class="text-muted">First Air: ${show.first_air_date}</small>
+                </p>
+            </div>
+        `;
+
+        document.querySelector('#popular-shows').appendChild(showElement);
+    })
+}
+
 // App init
 function init() {
     switch (global.currentPage) {
@@ -69,7 +111,7 @@ function init() {
             displayPopularMovies();
             break;
         case "/shows.html":
-            console.log("Shows page");
+            displayPopularShows();
             break;
         case "/tv-details.html":
             console.log("TV Details page");
